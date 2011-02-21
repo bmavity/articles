1d90fa5427660c3c49a6b1250cd0f8a9f70b558f
Brian Mavity <brian@brianmavity.com>
2011-02-21 16:28:42 -0600

    Modified httpServer example to return 404 instead of 500

--- nodeIntro/httpServer/server.js
+++ nodeIntro/httpServer/server.js
@@ -2,11 +2,11 @@ var http = require('http'),
     fs = require('fs');
 
 var respondToHttpRequest = function(req, res) {
-  var fileName = __dirname + '/flyingMonkeys.html';
+  var fileName = __dirname + req.url;
   fs.readFile(fileName, function(err, file) {
     if(err) {
-      res.writeHead(500, { 'Content-Type': 'text/plain' });
-      res.write('The error has occurred.');
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.write('File not found.');
       res.end();
     } else {
       res.writeHead(200, { 'Content-Type': 'text/html' });
